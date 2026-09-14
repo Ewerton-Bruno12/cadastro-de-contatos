4f3e1a0c196425be5f33f0ec5533837abd828a11
Ewerton Bruno <ewerton12042006@gmail.com>
2026-09-14 00:54:46 -0300

    feat: adiciona metodo de busca por nome no repositorio

--- src/main/java/br/com/ewerton/cadastro_de_contatos/repository/IContactRepository.java
+++ src/main/java/br/com/ewerton/cadastro_de_contatos/repository/IContactRepository.java
@@ -3,5 +3,9 @@ package br.com.ewerton.cadastro_de_contatos.repository;
 import br.com.ewerton.cadastro_de_contatos.model.ContactEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IContactRepository extends JpaRepository<ContactEntity, Long> {
+
+    List<ContactEntity> findByNameContainingIgnoreCase(String name);
 }
