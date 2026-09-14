74f501837a33bb53c079767e0472d254f750b28c
Ewerton Bruno <ewerton12042006@gmail.com>
2026-09-14 00:00:33 -0300

    Primeiro commit do projeto

---
+++ src/main/java/br/com/ewerton/cadastro_de_contatos/CadastroDeContatosApplication.java
@@ -0,0 +1,13 @@
+package br.com.ewerton.cadastro_de_contatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeContatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ewerton/cadastro_de_contatos/CadastroDeContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ewerton.cadastro_de_contatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
