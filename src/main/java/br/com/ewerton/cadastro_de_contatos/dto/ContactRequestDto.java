98bc817db7fb6fd180f8d0a71852c885d55a9605
Ewerton Bruno <ewerton12042006@gmail.com>
2026-09-24 19:33:24 -0300

    feat: adiciona validacao do formato do telefone

--- src/main/java/br/com/ewerton/cadastro_de_contatos/dto/ContactRequestDto.java
+++ src/main/java/br/com/ewerton/cadastro_de_contatos/dto/ContactRequestDto.java
@@ -2,12 +2,17 @@ package br.com.ewerton.cadastro_de_contatos.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record ContactRequestDto (
     @NotBlank(message = "Nome é obrigatório")
     String name,
     @Email(message = "Email inválido")
     String email,
+    @Pattern(
+            regexp = "^\\d{10,11}$",
+            message = "Telefone inválido. Use apenas números com DDD (10 ou 11 dígitos)"
+    )
     String phone,
     String address
 ) {}
